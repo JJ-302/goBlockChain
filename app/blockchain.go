e47ac355bd2653eb9dee31c38440f240efeb7047
Jun <>
2019-09-02 20:03:52 +0900

    move function 'printblock' to utils.go

--- app/blockchain.go
+++ app/blockchain.go
@@ -4,9 +4,6 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -63,25 +60,4 @@ func AddTransaction(senderBlockchainAddress string, recipientAddress string, val
 		Value:                   value,
 	}
 	transactionPool = append(transactionPool, ts)
-}
-
-// Printblock is print blockchain on terminal.
-func Printblock() {
-	headerLine := strings.Repeat("=", 25)
-	formatA := "%-15s : %v\n"
-	formatB := "%-25s : %v\n"
-	for i, v := range Chain {
-		fmt.Println(headerLine + "Chain" + strconv.Itoa(i) + headerLine)
-		fmt.Printf(formatA, "PreviousHash", v.PreviousHash)
-		fmt.Printf(formatA, "Timestamp", v.Timestamp.Format(time.RFC3339))
-		fmt.Printf(formatA, "Nonce", v.Nonce)
-		fmt.Println("Transactions")
-		fmt.Println(strings.Repeat("-", 50))
-		for _, v := range v.Transactions {
-			fmt.Printf(formatB, "SenderBlockchainAddress", v.SenderBlockchainAddress)
-			fmt.Printf(formatB, "RecipientAddress", v.RecipientAddress)
-			fmt.Printf(formatB, "Value", v.Value)
-		}
-	}
-	fmt.Printf("%s\n\n\n", strings.Repeat("*", 50))
 }
--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	app.Printblock()
+	utils.Printblock()
 
 	app.AddTransaction("A", "B", 1.0)
 	previousHash := app.Chain[len(app.Chain)-1].Hash()
 	app.CreateBlock(1, previousHash)
-	app.Printblock()
+	utils.Printblock()
 
 	app.AddTransaction("C", "D", 2.0)
 	app.AddTransaction("E", "F", 3.0)
 	previousHash = app.Chain[len(app.Chain)-1].Hash()
 	app.CreateBlock(2, previousHash)
-	app.Printblock()
+	utils.Printblock()
 }
