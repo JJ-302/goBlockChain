7fd7bde80b7a4d31013a8d9ef311dedc6b2a211c
Jun <>
2019-09-12 21:52:26 +0900

    add function 'generateBlockchainAddress'

--- app/wallet.go
+++ app/wallet.go
@@ -15,3 +15,37 @@ func createKeyPair() *ecdsa.PrivateKey {
 	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	return privateKey
 }
+
+// GenerateBlockchainAddress is return blockchainAddress.
+// blockchainAddress is created by publicKey.
+func GenerateBlockchainAddress() string {
+	privateKey := createKeyPair()
+	publicKey := privateKey.PublicKey
+	publicKeyByte := elliptic.Marshal(publicKey.Curve, publicKey.X, publicKey.Y)
+	sha256Encoder := sha256.New()
+	sha256Encoder.Write(publicKeyByte)
+	sha256Digest := sha256Encoder.Sum(nil)
+
+	ripemd160Encoder := ripemd160.New()
+	ripemd160Encoder.Write(sha256Digest)
+	ripemd160Digest := ripemd160Encoder.Sum(nil)
+
+	networkByte := []byte("11")
+	networkBitcoinPubKey := append(networkByte, ripemd160Digest...)
+	networkBitcoinPubKeyHex := hex.EncodeToString(networkBitcoinPubKey)
+
+	sha256Encoder2 := sha256.New()
+	sha256Encoder2.Write(networkBitcoinPubKey)
+	sha256Digest = sha256Encoder2.Sum(nil)
+	sha256Encoder2.Write(sha256Digest)
+	sha256Digest2 := sha256Encoder2.Sum(nil)
+	sha256Hex := hex.EncodeToString(sha256Digest2)
+
+	checksum := sha256Hex[:8]
+
+	addressHex := networkBitcoinPubKeyHex + checksum
+
+	blockchainAddress := base64.StdEncoding.EncodeToString([]byte(addressHex))
+
+	return blockchainAddress
+}
