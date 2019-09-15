d5d6a25dc02de716c797187497b14b68d038bbba
Jun <>
2019-09-15 22:26:49 +0900

    add constructor and method 'addTransaction'

--- app/transaction.go
+++ app/transaction.go
@@ -1,15 +1,12 @@
 package app
 
 import (
-	"crypto/ecdsa"
 	"crypto/sha256"
 	"encoding/json"
 )
 
 // Transaction is bitcoin's transaction info.
 type Transaction struct {
-	SenderPrivateKey ecdsa.PrivateKey
-	SenderPublicKey  ecdsa.PublicKey
 	SenderAddress    string
 	RecipientAddress string
 	Value            float64
@@ -20,4 +17,28 @@ func (tx *Transaction) hash() []byte {
 	sha256Encoder := sha256.New()
 	hash := sha256Encoder.Sum(txByte)
 	return hash
+}
+
+// AddTransaction returns result whether appending transaction successed.
+func (tx *Transaction) AddTransaction(wallet *Wallet) bool {
+	if tx.SenderAddress == MiningSender {
+		TransactionPool = append(TransactionPool, *tx)
+		return true
+	}
+	signature := wallet.GenerateSignature(tx.RecipientAddress, tx.Value)
+	if signature.verifyTransactionSignature(&wallet.PublicKey, *tx) {
+		TransactionPool = append(TransactionPool, *tx)
+		return true
+	}
+	return false
+}
+
+// CreateTransaction returns new trunsaction struct.
+func CreateTransaction(senAdd string, recAdd string, val float64) *Transaction {
+	tx := Transaction{
+		SenderAddress:    senAdd,
+		RecipientAddress: recAdd,
+		Value:            val,
+	}
+	return &tx
 }
