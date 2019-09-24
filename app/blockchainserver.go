cc1dcbe2d4d5ada1bb266541feb83339c12e1ea4
Jun <>
2019-09-24 20:38:39 +0900

    add function 'createWalletHandler'
    it returns blockchainAddress, PrivateKey and PublicKey

--- app/blockchainserver.go
+++ app/blockchainserver.go
@@ -3,6 +3,7 @@ package app
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,8 +16,17 @@ func init() {
 	CreateBlock(5, hex.EncodeToString(hash[:]), TransactionPool)
 }
 
+func createWalletHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	wallet := CreateWallet()
+	jsonValue, _ := json.Marshal(wallet)
+	w.Write(jsonValue)
+}
+
 // StartBlockchainServer start blockchain node.
 func StartBlockchainServer() error {
 	log.Println("Port:8080 to listen on")
+	http.HandleFunc("/wallet", createWalletHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
