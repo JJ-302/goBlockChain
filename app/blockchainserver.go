d84f97d1cb9ccdd3ffb72e09afcbc246dab63db5
Jun <>
2019-09-24 20:39:45 +0900

    add function 'getChainHandler'
    it returns current chain value

--- app/blockchainserver.go
+++ app/blockchainserver.go
@@ -5,15 +5,25 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"projects/goBlockChain/config"
 )
 
+var templates = template.Must(template.ParseFiles("app/views/chain.html"))
+
 func init() {
 	var initialHash []byte
 	hash := sha256.Sum256(initialHash)
 	CreateBlock(5, hex.EncodeToString(hash[:]), TransactionPool)
+}
+
+func getChainHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.Execute(w, Chain)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func createWalletHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,5 +38,6 @@ func createWalletHandler(w http.ResponseWriter, r *http.Request) {
 func StartBlockchainServer() error {
 	log.Println("Port:8080 to listen on")
 	http.HandleFunc("/wallet", createWalletHandler)
+	http.HandleFunc("/chain", getChainHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
