74bdecb18802c7b0cbd408cf5f48b899aa567105
Jun <>
2019-10-01 21:22:28 +0900

    add function 'findNeighbours'

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"projects/goBlockChain/app"
 	"projects/goBlockChain/config"
@@ -13,6 +14,9 @@ func main() {
 
 	port := flag.Int("int", 8080, "port")
 	flag.Parse()
+
+	addr := utils.GetHost()
+	fmt.Println(utils.FindNeighbours(addr, *port, 0, 2, 8080, 8082))
 
 	minerWallet := app.CreateWallet()
 	go app.StartMining(minerWallet)
--- utils/utils.go
+++ utils/utils.go
@@ -2,11 +2,57 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
 	"projects/goBlockChain/app"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
+
+// GetHost returns localhost address.
+func GetHost() string {
+	host, _ := os.Hostname()
+	addr, _ := net.LookupHost(host)
+	return addr[0]
+}
+
+func isFoundHost(target, port string) bool {
+	addr := target + ":" + port
+	_, err := net.DialTimeout("tcp", addr, time.Duration(1)*time.Second)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+// FindNeighbours search neighbours IP address and returns slice of it.
+func FindNeighbours(myHost string, myPort, startIPRange, endIPRange, startPort, endPort int) []string {
+	var neighbours []string
+	regexIP := regexp.MustCompile("^(\\d{1,3}.\\d{1,3}.\\d{1,3}.)(\\d{1,3})$")
+	result := regexIP.FindStringSubmatch(myHost)
+	if len(result) == 0 {
+		return neighbours
+	}
+	startIP := result[1]
+	lastIP, _ := strconv.Atoi(result[2])
+
+	addr := myHost + ":" + strconv.Itoa(myPort)
+
+	for guessPort := startPort; guessPort <= endPort; guessPort++ {
+		for ipRange := startIPRange; ipRange <= endIPRange; ipRange++ {
+			guessHost := startIP + strconv.Itoa(lastIP+ipRange)
+			guessAddress := guessHost + ":" + strconv.Itoa(guessPort)
+			if isFoundHost(guessHost, strconv.Itoa(guessPort)) && guessAddress != addr {
+				neighbours = append(neighbours, guessAddress)
+			}
+		}
+	}
+	return neighbours
+}
 
 // Printblock is print blockchain on terminal.
 func Printblock() {
